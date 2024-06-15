d211c9d44396181c1d96a9d5d4e459fe9c5ee29e
Sufiyan <StarKhan6368@gmail.com>
2024-06-15 17:54:41 +0530

    Updated Dependencies and rebuild.

---
+++ frontend/threaddit/dist/assets/SavedPosts-Dz2ZPfT8.js
@@ -0,0 +1,1 @@
+import{r as t,j as e}from"./index-Be8K0jJq.js";import{I as r}from"./InfinitePosts-BR17nx9P.js";import"./Post-qweXuIAK.js";function o(){return t.useEffect(()=>(document.title="Threaddit | saved",()=>{document.title="Threaddit"})),e.jsx("div",{className:"flex items-center p-2 w-full",children:e.jsx(r,{apiQueryKey:"saved",linkUrl:"posts/saved",forSaved:!0})})}export{o as default};
