2dd0bf11ff7224c58d37c688b752dd44570a1d02
Riki Phukon <rikiphukon17@gmail.com>
2024-07-09 17:07:01 +0000

    build

--- frontend/threaddit/vite.config.js
+++ frontend/threaddit/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000/",
+        target: "https://threaddit.onrender.com/",
         changeOrigin: true,
         secure: false,
         ws: true,
