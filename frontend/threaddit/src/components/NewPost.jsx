5f86e44f2313545cb3eb51628ea934e4bbea8187
Sufiyan <StarKhan6368@gmail.com>
2024-07-17 20:43:26 +0530

    Fix Post Creation, fixes Issue #4

--- frontend/threaddit/src/components/NewPost.jsx
+++ frontend/threaddit/src/components/NewPost.jsx
@@ -26,37 +26,40 @@ export default function NewPost({ setShowModal, isEdit = false, postInfo = {}, t
   const [imageUrl, setImageUrl] = useState("");
   const [thread, setThread] = useState(isEdit ? { id: threadInfo.thread_id, name: threadInfo.thread_name } : false);
   const { user } = AuthConsumer();
-  const { mutate: handleSubmit, status } = useMutation(async (e) => {
-    e?.preventDefault();
-    const formData = new FormData();
-    formData.append("title", title);
-    formData.append("content_type", mediaType);
-    formData.append("content_url", imageUrl);
-    formData.append("content", content);
-    if (media) {
-      formData.append("media", media, media.name);
-    }
-    formData.append("subthread_id", thread.id);
-    if (!isEdit) {
-      await axios
-        .post("/api/post", formData, { headers: { "Content-Type": "multipart/form-data" } })
-        .then(() => setShowModal(false))
-        .catch((err) => alert(`${err.message} check your fields, Title is mandatory`));
-    } else {
-      await axios
-        .patch(`/api/post/${postInfo.id}`, formData, { headers: { "Content-Type": "multipart/form-data" } })
-        .then((res) => {
-          queryClient.setQueryData({ queryKey: ["post/comment", `${postInfo.id}`] }, (oldData) => {
-            return { ...oldData, post_info: res.data.new_data };
-          });
-          setShowModal(false);
-        })
-        .catch((err) => alert(`${err.message} check your fields, Title is mandatory`));
+  const { mutate: handleSubmit, status } = useMutation({
+    mutationFn: async (e) => {
+      e?.preventDefault();
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("content_type", mediaType);
+      formData.append("content_url", imageUrl);
+      formData.append("content", content);
+      if (media) {
+        formData.append("media", media, media.name);
+      }
+      formData.append("subthread_id", thread.id);
+      console.log(formData);
+      if (!isEdit) {
+        await axios
+          .post("/api/post", formData, { headers: { "Content-Type": "multipart/form-data" } })
+          .then(() => setShowModal(false))
+          .catch((err) => alert(`${err.message} check your fields, Title is mandatory`));
+      } else {
+        await axios
+          .patch(`/api/post/${postInfo.id}`, formData, { headers: { "Content-Type": "multipart/form-data" } })
+          .then((res) => {
+            queryClient.setQueryData({ queryKey: ["post/comment", `${postInfo.id}`] }, (oldData) => {
+              return { ...oldData, post_info: res.data.new_data };
+            });
+            setShowModal(false);
+          })
+          .catch((err) => alert(`${err.message} check your fields, Title and thread is mandatory`));
+      }
     }
   });
   return (
-    <div className="flex flex-col p-5 space-y-5 w-5/6 h-4/6 rounded-md blur-none md:w-3/4 md:h-5/6 md:p-10 bg-theme-cultured">
-      <div className="flex flex-col justify-between items-center p-4 space-y-3 bg-white rounded-xl md:flex-row md:space-y-0">
+    <div className="flex flex-col w-5/6 p-5 space-y-5 rounded-md h-4/6 blur-none md:w-3/4 md:h-5/6 md:p-10 bg-theme-cultured">
+      <div className="flex flex-col items-center justify-between p-4 space-y-3 bg-white rounded-xl md:flex-row md:space-y-0">
         <div className="flex items-center space-x-3">
           <p>{isEdit ? "Editing" : "Posting"} as</p>
           <img src={user.avatar || avatar} className="object-cover w-8 h-8 rounded-full md:w-12 md:h-12" alt="" />
@@ -78,6 +81,7 @@ export default function NewPost({ setShowModal, isEdit = false, postInfo = {}, t
       </div>
       <form
         encType="multipart/form-data"
+        onSubmit={handleSubmit}
         className="flex flex-col flex-1 justify-around p-1.5 w-full h-1/2 bg-white rounded-md">
         <label htmlFor="title">
           <span>Title</span>
@@ -101,7 +105,7 @@ export default function NewPost({ setShowModal, isEdit = false, postInfo = {}, t
           </button>
           <div className="flex flex-col space-y-2">
             {preMd ? (
-              <div className="overflow-auto p-2 max-w-full h-28 border border-gray-800 prose">
+              <div className="max-w-full p-2 overflow-auto prose border border-gray-800 h-28">
                 <Markdown options={{ forceBlock: true, wrapper: "div" }} className="w-full">
                   {content.replace("\n", "<br />\n") || "This is markdown preview"}
                 </Markdown>
@@ -112,14 +116,14 @@ export default function NewPost({ setShowModal, isEdit = false, postInfo = {}, t
                 onChange={(e) => setContent(e.target.value)}
                 name="content"
                 id="content"
-                className="p-2 w-full h-28 border border-gray-800 md:max-h-32 focus:outline-none"
+                className="w-full p-2 border border-gray-800 h-28 md:max-h-32 focus:outline-none"
               />
             )}
           </div>
         </label>
         <label htmlFor="media" className="flex flex-col items-center space-y-3 md:space-y-0 md:space-x-5 md:flex-row">
           <select
-            className="px-10 py-2 bg-white rounded-md border md:px-12"
+            className="px-10 py-2 bg-white border rounded-md md:px-12"
             name="mediaType"
             id="mediaType"
             onChange={(e) => setMediaType(e.target.value)}>
@@ -149,7 +153,7 @@ export default function NewPost({ setShowModal, isEdit = false, postInfo = {}, t
               type="text"
               name="media_url"
               id="media_url"
-              className="p-2 w-full rounded-md border border-gray-800 focus:outline-none"
+              className="w-full p-2 border border-gray-800 rounded-md focus:outline-none"
               onChange={(e) => setImageUrl(e.target.value)}
             />
           )}
@@ -160,7 +164,7 @@ export default function NewPost({ setShowModal, isEdit = false, postInfo = {}, t
           </span>
         )}
         <button
-          onClick={handleSubmit}
+          type="submit"
           disabled={status === "loading"}
           className="py-2 font-semibold text-white rounded-md bg-theme-orange active:scale-95">
           {status === "loading" ? "Submitting..." : "Submit"}
